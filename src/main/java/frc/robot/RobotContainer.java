e46497d5dc01d4239283a19ee409f63b7cb0c06c
Mingle Li <limingle5@gmail.com>
2021-02-23 19:44:23 -0500

    Adding new drivetrain subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,14 +7,26 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.nio.file.Path;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.RamseteController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj.trajectory.TrajectoryUtil;
+import edu.wpi.first.wpilibj.trajectory.constraint.DifferentialDriveVoltageConstraint;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RamseteCommand;
 import frc.lib.controllers.BobXboxController;
 import frc.lib.util.DriveSignal;
 import frc.robot.commandgroups.SixBallAutoCommand;
@@ -40,6 +52,7 @@ import frc.robot.subsystems.Telescope;
 import frc.robot.subsystems.ColorMatcher;
 import frc.robot.subsystems.ColorSpinner;
 import frc.robot.subsystems.Conveyor;
+import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.IntakeArm;
@@ -48,10 +61,6 @@ import frc.robot.subsystems.TrajectoryGenerator;
 import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.Winch;
 import frc.robot.subsystems.Drivetrain.DriveMode;
-
-import jaci.pathfinder.Trajectory;
-import jaci.pathfinder.Waypoint;
-
 
 /**
 * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -64,6 +73,7 @@ public class RobotContainer {
 	public final ColorMatcher colorMatcher = new ColorMatcher();
 	public final ColorSpinner colorSpinner = new ColorSpinner();
 	public final Drivetrain drivetrain = new Drivetrain();
+	public final DriveSubsystem new_dt = new DriveSubsystem();
 	public final Shooter shooter = new Shooter();
 	public final Banana banana = new Banana();
 	public final Feeder feeder = new Feeder();
@@ -85,10 +95,10 @@ public class RobotContainer {
 		configureDriverBindings();
 		configureSubsystemBindings();
 		SmartDashboard.putNumber("RPM", RobotConstants.FLYWHEEL_PRESET_LINE);
-
+		
 		// TrajectoryGenerator.exportTrajectories(sixBall, "MiddleToTrench");
 	}
-
+	
 	public void stopAll() {
 		CommandScheduler.getInstance().cancelAll();
 		shooter.slowDown();
@@ -105,55 +115,55 @@ public class RobotContainer {
 		// driverController.bButton.whenPressed(new AngleIntakeCommand(intake, RobotConstants.INTAKE_POSITION_RETRACTED));
 		driverController.yButton.whenPressed(new ResetGyroAngle(drivetrain));
 		driverController.xButton.whenPressed(new ChangeDriveMode(drivetrain));
-
+		
 		driverController.rightBumper.whileActiveContinuous(new AngleIntakeCommand(intakeArm, false));
 		driverController.leftBumper.whileActiveContinuous(new AngleIntakeCommand(intakeArm, true));
 		
 		driverController.rightTriggerButton.whileActiveContinuous(new IntakeBallCommand(intakeArm));
 		driverController.leftTriggerButton.whileActiveContinuous(new IntakeBallCommand(intakeArm));
-
+		
 		// driverController.Dpad.Right.whileHeld(new IntakeBallCommand(intake, 0.2, false));
 		// driverController.Dpad.Left.whileHeld(new IntakeBallCommand(intake, 0.2, true));
 		// driverController.Dpad.Left.whenPressed(new RampShooterCommand(shooter, banana, RobotConstants.FLYWHEEL_PRESET_TRENCH));
 		// driverController.Dpad.Right.whileHeld();
 		
-
+		
 		// driverController.startButton.whenPressed(new ZeroIntakeCommand(intake));
 		//   trajectoryGenerator.getTrajectorySet().sideStartToNearScale.left, (DrivetrainTrajectory)drivetrain));
 	}
-
+	
 	private void configureSubsystemBindings() {
 		// subsystemController.aButton.whenPressed(new RampShooterCommand(shooter, 3750));
 		// subsystemController.aButton.whenReleased(new RampShooterCommand(shooter, 0));
-
+		
 		subsystemController.Dpad.Up.whenPressed(new RampShooterCommand(shooter, vision, banana, feeder, 3000.00));
 		subsystemController.Dpad.Down.whenPressed(new RampShooterCommand(shooter, vision, banana, feeder, 0.0));
-
+		
 		subsystemController.aButton.whenPressed(new InstantCommand(vision::getDistanceToTarget, vision));
 		subsystemController.bButton.whenPressed(new InstantCommand(vision::disableTracking, vision));
-
+		
 		// subsystemController.aButton.whenPressed(new TurnAndShootCommandSemiAuto(vision, drivetrain, feeder, conveyor, intakeArm , shooter, banana));
 		// subsystemController.aButton.whenPressed(new MoveBananaCommand(banana, 1000));
 		// subsystemController.bButton.whenPressed(new MoveBananaCommand(banana, 2500));
 		// subsystemController.bButton.whileHeld(new WinchMoveCommand(winch));
 		// subsystemController.yButton.whenPressed(new MoveBananaCommand(banana, 0));
 		// subsystemController.yButton.whenPressed();
-
+		
 		// subsystemController.Dpad.Up.whileHeld(new ElevatorMoveCommand(telescope, false));
 		// subsystemController.Dpad.Down.whileHeld(new ElevatorMoveCommand(telescope, true));
-
+		
 		// subsystemController.Dpad.Up.whenPressed(new RampShooterCommand(shooter, banana, 3650));
 		// subsystemController.Dpad.Right.whenPressed(new RampShooterCommand(shooter, banana, RobotConstants.FLYWHEEL_PRESET_TRENCH));
 		// subsystemController.Dpad.Left.whenPressed(new RampShooterCommand(shooter, banana, RobotConstants.FLYWHEEL_PRESET_BEHINDCOLORWHEEL));
 		
 		subsystemController.rightBumper.whileActiveContinuous(new MoveConveyorCommand(conveyor, feeder, shooter, 0.420, false));
 		subsystemController.leftBumper.whileActiveContinuous(new MoveConveyorCommand(conveyor, feeder, shooter, 0.420, true));
-
+		
 		subsystemController.rightTriggerButton.whileActiveContinuous(new FeedShooterCommand(feeder, shooter, false));
 		subsystemController.leftTriggerButton.whileActiveContinuous(new FeedShooterCommand(feeder, shooter, true));
 		
 		subsystemController.startButton.whileActiveContinuous(new ReverseShooterCommand(shooter));
- 		
+		
 	}
 	
 	/**
@@ -165,6 +175,48 @@ public class RobotContainer {
 		return new DriveFollowTrajectory(drivetrain, Trajectories.SnakeCurve[0], Trajectories.SnakeCurve[1], false);
 		// return new DriveFollowTrajectory(drivetrain, Trajectories.MoveOneMeter[0], Trajectories.MoveOneMeter[1], false);
 		// return new SixBallAutoCommand(vision, drivetrain, feeder, conveyor, intakeArm, shooter, banana);
+	}
+	
+	public Command getRamseteCommand() {
+		// Create a voltage constraint to ensure we don't accelerate too fast
+		var autoVoltageConstraint =
+		new DifferentialDriveVoltageConstraint(
+		new SimpleMotorFeedforward(RobotConstants.ksVolts,
+		RobotConstants.kvVoltSecondsPerMeter,
+		RobotConstants.kaVoltSecondsSquaredPerMeter),
+		RobotConstants.kDriveKinematics,
+		10);
+		
+		String trajectoryJSON = "paths/YourPath.wpilib.json";
+		Trajectory trajectory = new Trajectory();
+		try {
+			Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
+			trajectory = TrajectoryUtil.fromPathweaverJson(trajectoryPath);
+		} catch (IOException ex) {
+			DriverStation.reportError("Unable to open trajectory: " + trajectoryJSON, ex.getStackTrace());
+		}
+		
+		RamseteCommand ramseteCommand = new RamseteCommand(
+		trajectory,
+		new_dt::getPose,
+		new RamseteController(RobotConstants.kRamseteB, RobotConstants.kRamseteZeta),
+		new SimpleMotorFeedforward(RobotConstants.ksVolts,
+		RobotConstants.kvVoltSecondsPerMeter,
+		RobotConstants.kaVoltSecondsSquaredPerMeter),
+		RobotConstants.kDriveKinematics,
+		new_dt::getWheelSpeeds,
+		new PIDController(RobotConstants.kPDriveVel, 0, 0),
+		new PIDController(RobotConstants.kPDriveVel, 0, 0),
+		// RamseteCommand passes volts to the callback
+		new_dt::tankDriveVolts,
+		new_dt
+		);
+		
+		// Reset odometry to the starting pose of the trajectory.
+		new_dt.resetOdometry(trajectory.getInitialPose());
+		
+		// Run path following command, then stop at the end.
+		return ramseteCommand.andThen(() -> new_dt.tankDriveVolts(0, 0));
 	}
 	
 }
